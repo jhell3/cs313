7fdcdf28455a683f95ee47e3775f9a81199c4646
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-09-28 15:35:31 -0600

    intro

--- web/intro.php
+++ web/intro.php
@@ -12,10 +12,16 @@
         <h1>Pokemon</h1>
         <div>
         <?php
-            echo "my favorite pokemon is definitly Mewtwo. he is freaking legit.";
+            echo "my favorite pokemon is definitly Mewtwo. he is legit.";
         ?>
         </div>
-        <img src="https://ae01.alicdn.com/kf/HTB1wZx9BOOYBuNjSsD4q6zSkFXaa/Full-Square-Diamond-5D-DIY-Diamond-Painting-Mewtwo-Pokemon-Embroidery-Cross-Stitch-Rhinestone-Mosaic-Painting-Home.jpg_640x640.jpg" alt="mewtwo">
-
+        <p>I would consider him one of the strongest pokemon. I think it is time that I do something dynamic though. </p>
+        <img id="button" src="https://ae01.alicdn.com/kf/HTB1wZx9BOOYBuNjSsD4q6zSkFXaa/Full-Square-Diamond-5D-DIY-Diamond-Painting-Mewtwo-Pokemon-Embroidery-Cross-Stitch-Rhinestone-Mosaic-Painting-Home.jpg_640x640.jpg" alt="mewtwo">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+        $("button").click(function(){
+           $("p").toggle();
+        });
+        </script>
     </body>
 </html>
