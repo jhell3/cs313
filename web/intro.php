e926da56d323a5d1df96676396e5759301d7ca86
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-09-28 15:37:41 -0600

    intro

--- web/intro.php
+++ web/intro.php
@@ -6,7 +6,7 @@
         <title>INFO</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="assign.css">
     </head>
     <body>
         <h1>Pokemon</h1>
@@ -15,12 +15,12 @@
             echo "my favorite pokemon is definitly Mewtwo. he is legit.";
         ?>
         </div>
-        <p>I would consider him one of the strongest pokemon. I think it is time that I do something dynamic though. </p>
+        <p id="para">I would consider him one of the strongest pokemon. I think it is time that I do something dynamic though. </p>
         <img id="button" src="https://ae01.alicdn.com/kf/HTB1wZx9BOOYBuNjSsD4q6zSkFXaa/Full-Square-Diamond-5D-DIY-Diamond-Painting-Mewtwo-Pokemon-Embroidery-Cross-Stitch-Rhinestone-Mosaic-Painting-Home.jpg_640x640.jpg" alt="mewtwo">
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
-        $("button").click(function(){
-           $("p").toggle();
+        $("#button").click(function(){
+           $("#para").toggle();
         });
         </script>
     </body>
