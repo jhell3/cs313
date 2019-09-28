ec6a70b708c71459b6761addc294e020116b0888
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-09-28 15:22:24 -0600

    intro

---
+++ web/intro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>INFO</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Pokemon</h1>
+        <?php
+            echo "my favorite pokemon is definitly Mewtwo. he is freaking legit.";
+        ?>
+
+        <img src="https://ae01.alicdn.com/kf/HTB1wZx9BOOYBuNjSsD4q6zSkFXaa/Full-Square-Diamond-5D-DIY-Diamond-Painting-Mewtwo-Pokemon-Embroidery-Cross-Stitch-Rhinestone-Mosaic-Painting-Home.jpg_640x640.jpg" alt="mewtwo">
+
+    </body>
+</html>
