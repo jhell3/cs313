a9b0474a4093ee9fc253a0e0592c12a32b124d65
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-01 15:35:28 -0600

    team 3

--- web/week3/team3.php
+++ web/week3/team3.php
@@ -12,7 +12,7 @@ $str = "<h1>Hello " . $_POST["name"] . "!</h1>\n" .
       "<p> You said:<br>" . $_POST["comments"] . "</p>" .
       "<p> You have visited:<br>";
 
-$continent = $_POST['continent'];
+$continent = $_POST["continent"];
 if(empty($continent)) {
   $str .= "<p>Welcome to Earth. You haven't visited anywhere.</p>";
 }
