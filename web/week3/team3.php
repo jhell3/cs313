361295e292691bca6a72a313bfc819bd68e29b4d
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-01 15:05:41 -0600

    team 3

--- web/week3/team3.php
+++ web/week3/team3.php
@@ -6,7 +6,10 @@
   </head>
   <body>
 <?php
-
+ echo "<h1>Hello " . $_POST["name"] . "!</h1>\n" .
+      "<h2>Your email is <a href=\"mailto:" . $_POST["email"] . "\">" . $_POST["email"] ."</a></h2>\n" .
+      "<p>Your major is " . $_POST["major"] . ".</p>" .
+      "<p> You said:<br>" . $_POST["comments"] . "</p>" ;
 ?>
    </body>
 </html>
