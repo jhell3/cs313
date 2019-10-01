d671b4e366798ec707ef548a1730aa302925e937
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-01 15:18:33 -0600

    team 3

--- web/week3/team3.php
+++ web/week3/team3.php
@@ -6,10 +6,23 @@
   </head>
   <body>
 <?php
- echo "<h1>Hello " . $_POST["name"] . "!</h1>\n" .
+string $str = "<h1>Hello " . $_POST["name"] . "!</h1>\n" .
       "<h2>Your email is <a href=\"mailto:" . $_POST["email"] . "\">" . $_POST["email"] ."</a></h2>\n" .
       "<p>Your major is " . $_POST["major"] . ".</p>" .
-      "<p> You said:<br>" . $_POST["comments"] . "</p>" ;
+      "<p> You said:<br>" . $_POST["comments"] . "</p>" .
+      "<p> You have visited:<br>";
+
+$continent = $_POST['continent'];
+if(empty($continent)) {
+  $str += "<p>Welcome to Earth. You haven't visited anywhere.</p>"
+}
+else {
+   $str += "<ul>\n";
+   foreach ($continent as $value) {
+      $str += "<li>" . $value . "</li>";
+   }
+   $str += "</ul>";
+}
 ?>
    </body>
 </html>
