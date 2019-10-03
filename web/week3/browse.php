b7b79e3362a604ea70893c2187200c6e8ee91864
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-03 15:27:33 -0600

    week3

--- web/week3/browse.php
+++ web/week3/browse.php
@@ -7,14 +7,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <link href="/simple_cart.css" rel="stylesheet">
+<!--         <link href="/simple_cart.css" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"
                 integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
                  crossorigin="anonymous">
         </script>
         <script src="/jQuery.SimpleCart.js" ></script>
         <link rel="stylesheet" href="bootstrap.min.css">
-        <link rel="stylesheet" href="fontawesome.min.css">
+        <link rel="stylesheet" href="fontawesome.min.css"> -->
 
     </head>
     <body>
@@ -57,7 +57,7 @@
             }
          ?>
 
-        
+<!--     
         <script>
             $(document).ready(function () {
                 $('#cart').simpleCart();
@@ -72,11 +72,11 @@
                     showcartID : '#show-cart',
                     itemCountClass : '.item-count'
                 };
-            });
+            }); 
 
 
         </script>
-        
+         -->
     </body>
 </html>
 
