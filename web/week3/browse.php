4c1b660f3c17d46130651077f1e6d81666a2b5e8
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-03 18:44:54 -0600

    week3

--- web/week3/browse.php
+++ web/week3/browse.php
@@ -1,3 +1,19 @@
+<?php
+    session_start();
+
+    if(empty($_SESSION["cart"])){
+        $_SESSION["cart"]= array(
+            array("Poop on a Stick", 12.01, 0),
+            array("Fart Gun", 122.32, 0),
+            array("Pooperi, \"TacoBell\"", 13.21, 0),
+            array("Dog Breath Spray", 15.00, 0),
+            array("Moldy Sausage", 2.13, 0),
+            array("Laughing Lamas", 5.22, 0),
+            array("Grandma's Leftover Dinner....Dentures possibly included", 3.99, 0)
+        );
+    }
+
+?>
 <!DOCTYPE html>
     <head>
         <meta charset="utf-8">
@@ -7,45 +23,53 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <link href="simple_Cart.css" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-                integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"
-                 crossorigin="anonymous">
-        </script>
-        <script src="jQuery.SimpleCart.js" ></script>
-<!--         <link rel="stylesheet" href="bootstrap.min.css">
-        <link rel="stylesheet" href="fontawesome.min.css">  -->
 
     </head>
     <body>
     <h1>I Hate My Friend.</h1>
     <h3><a href="cart.php"> View Cart </a></h3>
-        <?php
-            $items = array("Poop on a Stick" => 12.01,
-                            "Fart Gun" => 122.32,
+
+
+
+<!-- "items" = array("Poop on a Stick" => 12.01,
+                                     "Fart Gun" => 122.32,
                             "Pooperi, \"TacoBell\"" => 13.21,
                             "Dog Breath Spray" => 15.00,
                             "Moldy Sausage" => 2.13,
                             "Laughing Lamas" => 5.22,
-                            "Grandma's Leftover Dinner....Dentures possibly included" => 3.99);
-        ?>
-
+                            "Grandma's Leftover Dinner....Dentures possibly included" => 3.99); -->
 <div id="cart"></div>
 
         <table>
             <tr>
                 <th>Objects of Prank War</th>
                 <th>Prices</th>
+                <th>Quantity</th>
                 <th>Add to Cart</th>
+
             </tr>
-        <?php
-            $i = 0;
+            <?php
+                for($x = 0; $x <= 7; $x++){
+                    echo"<tr>
+                        <td>" . $_SESSION["cart"][$x][0] . "</td>
+                        <td>" . $_SESSION["cart"][$x][1] . "</td>
+                        <td>" . $_SESSION["cart"][$x][2] . "</td>
+                        <td><button class=\"addCart\"
+                        data-name=\"$x\"
+                        data-price=\"$_SESSION[\"cart\"][$x][1]\"
+                        type=\"\">
+                        ADD To Cart
+                        </button></td></tr>"
+                }
+
+
+        /*    $i = 0;
             foreach($items as $x => $x_value){
                
                 echo "<tr> 
                         <td>" . $x . "</td> 
                         <td>" . $x_value . "</td> 
-                        <td><button class=\"sc-add-to-cart\"
+                        <td><button class=\"addCart\"
                         data-name=\"$i\"
                         data-price=\"$x_value\"
                         type=\"submit\">
@@ -53,30 +77,14 @@
                 </button></td>" . 
                      //  <td><button id= \"b[$x]\"> Add to Cart </button></td>
                     "</tr>";
-            $i += 1;
-            }
-         ?>
-
-<!--     
+            $i += 1; 
+            } */
+         ?>     
         <script>
-            $(document).ready(function () {
-                $('#cart').simpleCart();
-            });
-            
-            $(document).ready(function () {
-                $('#cart').simpleCart({
-                    addtoCartClass: '.sc-add-to-cart',
-                    cartProductListClass: '.cart-products-list',
-                    totalCartCountClass: '.total-cart-count',
-                    totalCartCostClass: '.total-cart-cost',
-                    showcartID : '#show-cart',
-                    itemCountClass : '.item-count'
-                };
-            }); 
 
 
         </script>
-         -->
+         
     </body>
 </html>
 
