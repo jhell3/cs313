1e20f59ea07d042d21a7d6b4209f4d10a3a17863
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-05 16:34:59 -0600

    week3

--- web/week3/cartpage.php
+++ web/week3/cartpage.php
@@ -29,6 +29,7 @@
             
             $y = 0;
             for($x = 0; $x < 7; $x++){
+                echo $_POST["item[$x]"];
                 if($_POST["item[$x]"] != 0){
                     $_SESSION["cart"][$x][2] += $_POST["item[$x]"];
                     echo $_POST["item[$x]"];
