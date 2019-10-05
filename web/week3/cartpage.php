b52902a7d959ea8fc8effe2888af129cf7eea448
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-05 16:51:05 -0600

    week3

--- web/week3/cartpage.php
+++ web/week3/cartpage.php
@@ -26,7 +26,7 @@
     <body>
     <h1><a href="browse.php">I Hate My Friend.</a></h1>
         <?php
-            $item = $_POST["item[0]"];
+            $item = $_POST["item0"];
             echo $item;
             $y = 0; 
             for($x = 0; $x < 7; $x++){
