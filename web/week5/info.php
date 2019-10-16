b4564d16e3086a7afe0e0fee9a755b16447c09db
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-15 19:31:40 -0600

    week 5

---
+++ web/week5/info.php
@@ -0,0 +1,67 @@
+<?php
+  #  require "dbConnect.php";
+   # $db = get_db();
+?>
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <table>
+            <tr>        
+                <th>Pokedex</th>
+                <th>Name</th>
+                <th>Type</th>
+            </tr>
+                
+        <?php
+        try
+        {
+          $dbUrl = getenv('DATABASE_URL');
+        
+          $dbOpts = parse_url($dbUrl);
+        
+          $dbHost = $dbOpts["host"];
+          $dbPort = $dbOpts["port"];
+          $dbUser = $dbOpts["user"];
+          $dbPassword = $dbOpts["pass"];
+          $dbName = ltrim($dbOpts["path"],'/');
+        
+          $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+          echo 'Error!: ' . $ex->getMessage();
+          die();
+        
+          $pokedex = $_GET[pokedex];
+        }
+            foreach ($db->query("SELECT * FROM pokemon WHERE pokedex = \'$pokedex\';") as $row){
+                echo "<tr>
+                        <td>" . $row['pokedex'] . "</td>
+                        <td>" . $row['name'] . "</td>
+                        <td>" . $row['type'] . "</td>
+                    </tr>";
+            }
+            echo "</table>";
+        ?>
+        <table>
+            <tr>
+                <th>HP</th>
+                <th>Attack</th>
+                <th>Defense</th>
+                <th>Sp. Attack</th>
+                <th>Sp. Defense</th>
+                <th>Speed</th>
+            </tr>
+        </table>
+        <script src="" async defer></script>
+    </body>
+</html>
