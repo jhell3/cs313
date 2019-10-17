878dc040ed44a531bdcebe3ea4228c47a675eedd
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-17 11:32:16 -0600

    5

--- web/week5/info.php
+++ web/week5/info.php
@@ -1,6 +1,6 @@
 <?php
-  #  require "dbConnect.php";
-   # $db = get_db();
+  require "dbConnect.php";
+ $db = get_db();
 ?>
 <!DOCTYPE html>
     <head>
@@ -20,33 +20,12 @@
             </tr>
                 
         <?php
-        try
-        {
-          $dbUrl = getenv('DATABASE_URL');
-        
-          $dbOpts = parse_url($dbUrl);
-        
-          $dbHost = $dbOpts["host"];
-          $dbPort = $dbOpts["port"];
-          $dbUser = $dbOpts["user"];
-          $dbPassword = $dbOpts["pass"];
-          $dbName = ltrim($dbOpts["path"],'/');
-        
-          $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        
-          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        catch (PDOException $ex)
-        {
-          echo 'Error!: ' . $ex->getMessage();
-          die();
-        }
-            
+       
            $pokedex = $_GET['pokedex'];
            $statement = $db->prepare("SELECT * FROM pokemon");
            $statement->execute();
            while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
-                    
+            if($row === $pokedex)
                     echo "<tr>
                         <td>" . $row['pokedex'] . "</td>
                         <td>" . $row['name'] . "</td>
