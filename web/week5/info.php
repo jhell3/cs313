8a7644db0d7bde39db1e5781cd919963ab0bee08
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-19 10:11:01 -0600

    5

--- web/week5/info.php
+++ web/week5/info.php
@@ -73,6 +73,7 @@
                         $name2 =  $row['name'];
                     }
                     echo "$name evolves into $name2 at level $evolve_at.<br>";
+                    
                     if($evolution_num == 1.3){
                         $pokedex++;
                         $statement = $db->prepare("SELECT * FROM pokemon where pokedex = $pokedex");
@@ -80,13 +81,14 @@
                         while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
                             $name3 =  $row['name'];
                         }
+                        $pokedex--;
                         $statement3 = $db->prepare("SELECT * FROM evolution where id = $pokedex");
                         $statement3->execute();
                         while ($row = $statement3->fetch(PDO::FETCH_ASSOC)){
                             $evolve_at2 = $row['evolve_at'];
                         }
                         echo "$name2 evolves into $name3 at level $evolve_at2";
-                        $pokedex--;
+                        
                     }
                     $pokedex--;
                     
