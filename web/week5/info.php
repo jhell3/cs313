3be97195bf04b7a8999dc02e9a16a4d868de33a6
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-19 18:00:52 -0600

    5

--- web/week5/info.php
+++ web/week5/info.php
@@ -10,6 +10,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="week5.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </head>
     <body>
         <h3><a href="index5.php">Return Home</a></h3>
@@ -127,6 +128,8 @@
                 ?>
 
     
-        <script src="" async defer></script>
+        <script>
+           // $(document)
+        </script>
     </body>
 </html>
