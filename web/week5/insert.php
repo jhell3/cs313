6cd54c557dffd1923fdade2c081c69007ac75697
josh Hellewell <Jhell@joshs-MacBook-Pro-2.local>
2019-10-25 10:06:24 -0600

    6

--- web/week5/insert.php
+++ web/week5/insert.php
@@ -1,35 +1,18 @@
 <?php
-  require "dbConnect.php";
+  require("dbConnect.php");
  $db = get_db();
  
-?>
 
-<!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="week5.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    </head>
-    <body>
-        <h3><a href="index5.php">Return Home</a><a href="addpokemon.php">Add another Pokemon</a></h3>
-    <form action="info.php" method="GET">
-            <p>Search by Pokedex number<input name="pokedex"><button type="submit">Search</button></p>
-    </form>
-    <?php
-        $name = POST_["name"];
-        $type = POST_["type"];
-        $hp = POST_["hp"];
-        $attack = POST_["attack"];
-        $defense = POST_["defense"];
-        $sp_attack = POST_["sp_attack"];
-        $sp_defense = POST_["sp_defense"];
-        $speed = POST_["speed"];
-        $evo_num = POST_["evo_num"];
-        $evo_at = POST_["evo_at"];
+        $name = POST_['name'];
+        $type = POST_['type'];
+        $hp = POST_['hp'];
+        $attack = POST_['attack'];
+        $defense = POST_['defense'];
+        $sp_attack = POST_['sp_attack'];
+        $sp_defense = POST_['sp_defense'];
+        $speed = POST_['speed'];
+        $evo_num = POST_['evo_num'];
+        $evo_at = POST_['evo_at'];
 
         echo $name;
         echo $type;
